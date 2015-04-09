0e73c19fd655103eba522fc1045476a8c8b55d81
Piyush Kumar <piyush.kmr301@gmail.com>
2015-04-09 20:32:01 +0530

    All files managed basic bugs removed

---
+++ src/facebook/data.js
@@ -0,0 +1,15 @@
+//posts to be shown on scrolling page
+var message=[
+	{"message":'<img src="https://fbcdn-sphotos-c-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/s720x720/1510921_666334870139961_4977699579703994989_n.jpg?oh=51b0d00562a55f9fbc0480674692fca4&oe=55B9014B&__gda__=1438191820_762b137eeca019b50446c3e7157d7980" class="fb-media">'},
+	{"message":'<img src="https://fbcdn-sphotos-e-a.akamaihd.net/hphotos-ak-xpt1/v/t1.0-9/11139383_666064630166985_1626728052663141151_n.jpg?oh=9fb231b8a6913762e0a57dc54a600421&oe=55B8CB13&__gda__=1437314327_4754e8ee95f8b97c3f75b7730f5a34fa" class="fb-media">'},
+	{"message":'<img src="https://fbcdn-sphotos-g-a.akamaihd.net/hphotos-ak-xta1/v/t1.0-9/s720x720/11115622_666049996835115_7828686650334386516_n.jpg?oh=0e4b6e1f6a1bcd93ef04c9468cd84f7b&oe=559CD7FC&__gda__=1436044681_1b4fa90fc402a61b6e0e322eb0112f11" class="fb-media">'},
+	{"message":'<img src="https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-xap1/v/t1.0-9/s720x720/11084261_660528270720621_7825239368322287057_n.jpg?oh=d7047007f2875e2d2baf4deb6c1306c4&oe=55A357CF&__gda__=1437360785_fd50357fbd8f4ae118e5b8df3b88ff76" class="fb-media">'},
+	{"message":'<img src="https://scontent-sin.xx.fbcdn.net/hphotos-xaf1/v/t1.0-9/s720x720/10712846_665688373537944_8922442568004902329_n.jpg?oh=17900d56d3d27af95c5f5f92a2818938&oe=559E286B" class="fb-media">'},
+	{"message":'<img src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xfa1/v/t1.0-9/s720x720/10382762_664487933657988_6376508233842491746_n.jpg?oh=ea5e503791cb617b7f0712aac0829cca&oe=559FF25B&__gda__=1436622473_fff00a0fffcaf7002c67f8a8b11293c1" class="fb-media">'}
+];
+//The URL of the page for like Button
+var pageURL="https://www.facebook.com/GREInsideOut";
+//The name to be shown as profile name
+var profileName="GREIO";
+//The profile Logo to be shown (relative or absolute)
+var profileLogo="img/greio.png"
---
+++ src/whatsapp/data.js
@@ -0,0 +1,26 @@
+//Group Name to be displayed on Whatsapp Header
+var groupName="GREIO";
+//The users name to be shown in the chat window
+var users=new Array("Jagjeet","Aman","Aayan","Ananya","Zaara","Ibrahim","Zaid","Joshua","David","Eric","Pranav","Krishan","Vishal","Gunjan","Rimmi","Aarti","Shashank","Nikhil","Sofia");
+//The chat data to be shown in groups
+//Users' name will be displayed randomly everytime the page is refreshed.
+var chat=[
+	{"user":"2",	"message":"Hey everyone"},
+	{"user":"0",	"message":"Hello"},
+	{"user":"3",	"message":"I am willing to know how to test my aptitude for GRE."},
+	{"user":"0",	"message":"You can give our Litmus Test which is free of cost"},
+	{"user":"1",	"message":"Yes, I got 300 marks in that. Hope its good enough."},
+	{"user":"0",	"message":"Yes, the marks are good for a start, but you need to gain more to score good in GRE"},
+	{"user":"4",	"message":"Of Course Things have to pretty much prepared to crack GRE"},
+	{"user":"0",	"message":"You can prepare from our courses and can have a demo before buying the courses."},
+	{"user":"5",	"message":"Oh Thats good..."},
+]
+//You can specify colors of users name here
+var colors={
+	"0":"#2B619A",
+	"1":"#00B159",
+	"2":"#D41243",
+	"3":"#F47835",
+	"4":"#F47735",
+	"5":"#00A600"
+}
